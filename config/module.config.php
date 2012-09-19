626bc07164bec2b9d4ac485baf546d1360702bd6
Alexander Tukai <atukai@gmail.com>
2012-09-19 12:54:30 +0300

    Add global default config into config dir

---
+++ config/module.config.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'phpsettings'   => array(
+        'display_startup_errors'        => false,
+        'display_errors'                => false,
+        'max_execution_time'            => 30,
+        'date.timezone'                 => 'Europe/Moscow',
+        'mbstring.internal_encoding'    => 'UTF-8',
+    ),
+);
